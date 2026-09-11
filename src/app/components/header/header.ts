c89825fc8d738dbb3c37c5676d88eb1b8897ed4d
Artem Mikhalenia <goldtree3000@gmail.com>
2026-09-11 16:50:52 +0300

    feat: add header bottom and some styles with animation

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { Badge } from '../badge/badge';
 // import { MatGridListModule } from '@angular/material/grid-list';
 // import { MatIconModule } from '@angular/material/icon';
 // import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [Badge],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
